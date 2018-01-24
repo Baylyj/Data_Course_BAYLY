ac22db4d4ec15003ffa87421d968239787cff8b9
unknown <jbayly175@gmail.com>
2018-01-24 09:14:10 -0700

    added

---
+++ DF practive.R
@@ -0,0 +1,13 @@
+setwd("~/R/Data_Course_Material/Data_Course/data/")
+list.files()
+df = read.csv("Fake_grade_data.csv")
+df$X = NULL
+row.names(df) = df$Student
+df$Student = NULL
+df$`Total Grade`= rowSums(df)
+
+A = df[df$`Total Grade` >= 700,]
+
+A[order(A$`Total Grade`, decreasing = TRUE),]
+
+sample(df$`Total Grade`)
