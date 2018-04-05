c8d7ec69ac833869b856c45e75303ffd7518143e
unknown <jbayly175@gmail.com>
2018-04-05 12:00:21 -0600

    exam 3

---
+++ Exam3/exam3.R
@@ -0,0 +1,36 @@
+library(ShortRead)
+library(vegan)
+
+
+setwd("R/Data_Course_Material/Data_Course/Exam3/")
+
+###Part1####
+#read and convert
+sample1 = readFastq("Sample1.fastq")
+sample2 = readFastq("Sample2.fastq")
+writeFasta(sample1,"Sample1.fasta")
+writeFasta(sample2,"Sample2.fasta")
+
+#number of sequences
+length(sample1)
+length(sample2)
+
+#trim to first 100 bases
+trimmed1 = narrow(sample1, start = 1, end = 100)
+trimmed2 = narrow(sample2, start = 1, end = 100)
+#Save
+writeFastq(trimmed1, "Sample1.fastq.trim")
+writeFastq(trimmed2, "Sample2.fastq.trim")
+
+###Part2####
+table = read.csv("exam3_otu_table.csv")
+meta = read.csv("exam3_metadata.csv")
+table[,1] -> rownames(table)
+table$OTU.ID = NULL
+meta[,1] -> rownames(meta)
+meta$SampleID = NULL
+meta$SampleType = as.factor(meta$SampleType)
+submeta = meta[rownames(meta[c(which(meta$SampleType == "Soil"),which(meta$SampleType == "rhizosphere")),]),]
+subtable = table[rownames(submeta)]
+
+#I then spent an hour trying to get adonis to work :(
